44df07e0f199f7f6e36d8616bf28cf561796a759
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-04-25 10:21:51 -0500

    Create HornForm.js

---
+++ lab1-app/src/Components/HornForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+
+{/*sort by horns*/}
+function HornForm() {
+      return (
+        <Form>
+
+        </Form>
+      );
+    }
+  
+  
+  export default HornForm;
